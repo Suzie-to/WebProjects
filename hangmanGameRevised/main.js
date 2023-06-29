e7efdbbfcd7a1408275a5389ecdd9133450db377
Suzie-to <yahiya.zyahiya@gmail.com>
2023-06-29 13:34:24 +0100

    replaceing each underscore with a letter, guess count

--- hangmanGameRevised/main.js
+++ hangmanGameRevised/main.js
@@ -6,6 +6,17 @@ let words = ["rabbit", "snail", "airport", "carrots", "river", "mountain", "hous
 "earth", "toothpaste", "daughter", "tongue", "popcorn", "lunch", "excercise", "carve", "stamp", "program", "haircut", "brush", "spell", "thirty", "twenty", "fourty", "fifty", 
 "sixty", "seventy", "eighty", "ninety", "hundred"]
 
+function isLetter(input) {
+    if (input.toLowerCase > 'a' && input.toLowerCase < 'z') {
+        //console.log("A Letter from the alphabet")
+        return true; 
+    }
+    else {
+        log.console(input + " IS NOT A LETTER. TRY AGAIN!!!");
+        return false;
+    }
+}
+
 const getRandomWord = (words) => {
     let randomIndex = Math.floor(Math.random() * words.length) // Math.random() * (max-min) + min incl min, excl max
     return words[randomIndex]
@@ -14,28 +25,74 @@ const getRandomWord = (words) => {
 // let randomWord = getRandomWord(words);
 // console.log(randomWord);
 
-const generateHiddenWord = (word) => {
+const generateunderscoreWord = (word) => {
     //transform the characters into underscores
-    let hiddenWord = word.split('').map(char => { return '_'}) 
-    return hiddenWord
+    let underscoreWord = word.split('').map(char => { return '_'}) 
+    return underscoreWord
 }
 
 
-// const generateHiddenWord2 = (word) => { //ALTERNATIVE TO MAP
-//     let hiddenWord = []
+// const generateunderscoreWord2 = (word) => { //ALTERNATIVE TO MAP
+//     let underscoreWord = []
 //     for(let i = 0; i <word.length; i++) {
-//         hiddenWord.push('_')
-//     }return hiddenWord;
+//         underscoreWord.push('_')
+//     }return underscoreWord;
 // }
-// let encrypted = generateHiddenWord(randomWord);
+// let encrypted = generateunderscoreWord(randomWord);
 //console.log(encrypted);
 
 const startGame = () => {
     let randomWord = getRandomWord(words);
-    let encrypted = generateHiddenWord(randomWord);
+    let underscoreWord = generateunderscoreWord(randomWord);
     //return the word as a string
-    console.log(encrypted.join(' '));
+    console.log(underscoreWord.join(' '));
 }
 
+
+//check whether the entered letter is in the current word
+//TODO check if word can be replaced with randomWord
+const isLetterInWord = (letter, word) => { 
+    return word.includes(letter);
+}
+
+const placeMatchingLetter = (letter, randomWord,underscoreWord) => {
+    for (let i = 0; i< randomWord.length; i++) {
+        let currentLetter = randomWord[i]
+        if (currentLetter === letter) {
+            underscoreWord[i] = letter;
+        }
+    }
+    return underscoreWord;
+}
+
+//console.log(placeMatchingLetter('l', 'hello', ['_', '_', '_', '_', '_']));
+
 startGame();
-const answer = readlineSync.question("Enter a Letter: ");
+let randomWord = getRandomWord(words);
+let underscoreWord = generateunderscoreWord(randomWord);
+//TODO rethink the number of guesses in relation to the length of the word
+let guesses = Math.floor(randomWord.length / 2);
+
+// loop to allow several guesses
+while (guesses > 0) {
+    console.log(underscoreWord.join(' '));
+    const answer = readlineSync.question("Enter a Letter: ");
+    console.log("You entered: ", answer);
+    //TODO incorporate error messages for more than 1 letter, no letter
+    // console.log(typeof answer);
+    console.log(guesses, " guesses left");
+    if(isLetterInWord(answer,randomWord)) {
+        console.log(`YAY, ${answer} is in ${randomWord}`);
+        //update the encrypted word by replacing the underscore at the position with the correctly guessed letter
+        underscoreWord = placeMatchingLetter(answer, randomWord, underscoreWord);
+    }
+    else {
+        console.log("NOT", randomWord);
+        guesses--;
+    }
+    
+}
+
+
+
+startGame();
