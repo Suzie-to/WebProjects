ec256374e7f5898d4eb8c3fbdb1ba4c4d82a85c5
Suzie-to <yahiya.zyahiya@gmail.com>
2023-07-08 12:38:34 +0100

    display category buttons

--- hangmanGameRevised/setup.js
+++ hangmanGameRevised/setup.js
@@ -1,5 +1,11 @@
 
 //******************************************************SET UP**************************************
+const userInput = document.getElementById("user-input");
+const newGame = document.getElementById("new-game-container");
+const newGameButton = document.getElementById("new-game-button");
+const canvas = document.getElementById("canvas");
+const endGameText = document.getElementById("end-game-text")
+const categories = document.getElementById("categories");
 
 const cssProperties = [
     'COLOR',
@@ -56,66 +62,83 @@ const cssProperties = [
     'STYLE',
     'META'
   ];
-let words = [cssProperties, jsFunctions, htmlTags];
 
-// return a random word from the list
-const getRandomWord = (words) => {
-    let randomIndex = Math.floor(Math.random() * words.length) // Math.random() * (max-min) + min incl min, excl max
-    return words[randomIndex]
+
+//display a button for each category 
+const displayCategories = () => {
+    categories.innerHTML += `<h3>What's your expertise?</h3>`;
+    //create a button 
+    let buttonContainer = document.createElement("div");
+    
+    //generate the word
+    for(let category in categories) {
+        buttonContainer.innerHTML += `<button class="options" onclick="generateWord('${category}')" >${value}</button>`
+    }
+
+    categories.appendChild(buttonContainer);
+};
+
+// // return a random word from the list
+const getRandomWord = (categories) => {
+    let categoryButtons = document.querySelectorAll(".categories")
 }
-
-// let randomWord = getRandomWord(words);
-// console.log(randomWord);
-
-// return the word as a string of underscores
-const generateunderscoreWord = (word) => {
-    //transform the characters into underscores
-    let underscoreWord = word.split('').map(char => { return '_'}) 
-    return underscoreWord;
-}
-// const generateunderscoreWord2 = (word) => {
-//     let underscoreWord = []
-//     for(let i = 0; i <word.length; i++) {
-//         underscoreWord.push('_')
-//     }return underscoreWord;
+// const getRandomWord = (words) => {
+//     let randomIndex = Math.floor(Math.random() * words.length) // Math.random() * (max-min) + min incl min, excl max
+//     return words[randomIndex]
 // }
-// let encrypted = generateunderscoreWord(randomWord);
-//console.log(encrypted);
 
-const startGame = () => {
-    // let playerName = readlineSync.question("Enter your name: ");
-    // ranking.push({ name: playerName, score: 0 });
-    let randomWord = getRandomWord(words);
-    let underscoreWord = generateunderscoreWord(randomWord);
-    //console.log(underscoreWord.join(' '));
-}
+// // let randomWord = getRandomWord(words);
+// // console.log(randomWord);
+
+// // return the word as a string of underscores
+// const generateunderscoreWord = (word) => {
+//     //transform the characters into underscores
+//     let underscoreWord = word.split('').map(char => { return '_'}) 
+//     return underscoreWord;
+// }
+// // const generateunderscoreWord2 = (word) => {
+// //     let underscoreWord = []
+// //     for(let i = 0; i <word.length; i++) {
+// //         underscoreWord.push('_')
+// //     }return underscoreWord;
+// // }
+// // let encrypted = generateunderscoreWord(randomWord);
+// //console.log(encrypted);
+
+// const startGame = () => {
+//     // let playerName = readlineSync.question("Enter your name: ");
+//     // ranking.push({ name: playerName, score: 0 });
+//     let randomWord = getRandomWord(words);
+//     let underscoreWord = generateunderscoreWord(randomWord);
+//     //console.log(underscoreWord.join(' '));
+// }
 
 
-//check whether the entered letter is in the current word
-const isLetterInWord = (letter, word) => { 
-    return word.includes(letter);
-}
+// //check whether the entered letter is in the current word
+// const isLetterInWord = (letter, word) => { 
+//     return word.includes(letter);
+// }
 
-// place the correctly guessed letter in the correct position
-const placeMatchingLetter = (letter, randomWord,underscoreWord) => {
-    for (let i = 0; i< randomWord.length; i++) {
-        let currentLetter = randomWord[i]
-        if (currentLetter === letter) {
-            underscoreWord[i] = letter;
-        }
-    }
-    return underscoreWord;
-}
+// // place the correctly guessed letter in the correct position
+// const placeMatchingLetter = (letter, randomWord,underscoreWord) => {
+//     for (let i = 0; i< randomWord.length; i++) {
+//         let currentLetter = randomWord[i]
+//         if (currentLetter === letter) {
+//             underscoreWord[i] = letter;
+//         }
+//     }
+//     return underscoreWord;
+// }
 
-// check whether the player has won
-const  hasWon = (sourceWord, newWord) => {
-    if (sourceWord === newWord.join('')) {
-        return true;
-    }
-    else {
-        return false;
-    }
+// // check whether the player has won
+// const  hasWon = (sourceWord, newWord) => {
+//     if (sourceWord === newWord.join('')) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
 
-}
+// }
 
-module.exports= {isLetterInWord, placeMatchingLetter, hasWon};
+// module.exports= {isLetterInWord, placeMatchingLetter, hasWon};
