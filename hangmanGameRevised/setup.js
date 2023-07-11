ba781e12913db589c3c691918e48a902b78492a5
Suzie-to <yahiya.zyahiya@gmail.com>
2023-07-11 11:21:47 +0100

    fixed popup

--- hangmanGameRevised/setup.js
+++ hangmanGameRevised/setup.js
@@ -1,11 +1,13 @@
 
 //******************************************************SET UP**************************************
+
+const categoriesContainer = document.getElementById("categories-container");
+const letterContainer = document.getElementById("letter-container");
 const userInput = document.getElementById("user-input");
 const newGameContainer = document.getElementById("new-game-container");
 const newGameButton = document.getElementById("new-game-button");
 const canvas = document.getElementById("canvas");
 const endGameText = document.getElementById("end-game-text")
-const categoriesContainer = document.getElementById("categories-container");
 
 const categories = {
     cssProperties : [
@@ -28,10 +30,10 @@ jsFunctions : [
     'CREATEELEMENT',
     'INNERHTML',
     'APPENDCHILD',
-    'CONSOLE.LOG',
+    'CONSOLE',
     'SETTIMEOUT',
     'PARSEINT',
-    'JSON.STRINGIFY'
+    'STRINGIFY'
   ],
 
  htmlTags : [
@@ -66,7 +68,7 @@ jsFunctions : [
 };
 
 const max_guesses = 6; // the amount of limbs o a stick figure
-let randomWord = "";
+
 let winsCount = 0;
 let lossesCount = 0;
 let secretWord = "";
@@ -85,7 +87,7 @@ const displayCategories = () => {
 };
 
 //diable a button that has been ckilcked already
-const blocker = () => {
+const disableButtons = () => {
     let categoryButtons = document.querySelectorAll(".categories");
     let letterButtons = document.querySelectorAll(".letters");
 
@@ -107,7 +109,8 @@ let displayWord = (categoryName) => {
     let categoryButtons = document.querySelectorAll(".categories");
 
     //convert the node list into an array to access the elements
-    Array.from(categoryButtons).array.forEach(button => {
+    Array.from(categoryButtons).forEach(button => {
+        //console.log("categoryName");
         //compare each element value(category) with the value(category) of the button innerText
         if(button.innerText.toLowerCase() === categoryName) {
             //finally highlight the button and activate it
@@ -130,13 +133,13 @@ let displayWord = (categoryName) => {
     let categoryArray = categories[categoryName];
     //genereate a random index : Math.random() * (max-min) + min incl min, excl max
     let randomIndex = Math.floor(Math.random() * categoryArray.length);
-    randomWord = categoryArray[randomIndex];
-    console.log(`SECRET WORD = ${randomWord}`);
+    secretWord = categoryArray[randomIndex];
+    console.log(`SECRET WORD = ${secretWord}`);
 
 
 //ENCRYPTION - REPLACE EACH CHARACTER WITH AN UNDERSCORE
-    //let maskedWord = randomWord.split('').map(() => '<span class="dashes">_</span>').join('');
-    let maskedWord = randomWord.replace(/./g, '<span class="dashes">_</span>');
+    //let maskedWord = secretWord.split('').map(() => '<span class="dashes">_</span>').join('');
+    let maskedWord = secretWord.replace(/./g, '<span class="dashes">_</span>');
     
     //DISPLAY THE UNDERCORE WORD
     userInput.innerHTML = maskedWord;
@@ -148,8 +151,8 @@ const startGame = () => {
 //RESET FIELD
     winsCount = 0;
     lossesCount = 0;
-    userInputSection.innerHTML = "";
-    optionsContainer.innerHTML = "";
+    userInput.innerHTML = "";
+    categoriesContainer.innerHTML = "";
     letterContainer.classList.add('hide');
     newGameContainer.classList.add('hide');
     letterContainer.innerHTML = "";
@@ -158,13 +161,12 @@ const startGame = () => {
     for(let i = 65; i < 91; i++) {
         let button = document.createElement("button");
         button.classList.add("letters");
-    }
     //cast char into string
     button.innerText = String.fromCharCode(i);
     letterContainer.append(button);
 
     button.addEventListener("click", () => {
-        let secretWordArray = secretWord.split('');
+        let secretWordArray = secretWord.split("");
         let dashes = document.getElementsByClassName("dashes");
         
         console.log(button.innerText);
@@ -182,64 +184,35 @@ const startGame = () => {
                         endGameText.innerHTML = `<h2 class="won-message">YOU'VE WON!!!</h2>
                                                 <p>The word was <span>${secretWord}</span></p>`;
                                             //block all buttons
-                        blocker();
+                        disableButtons();
                     }
 
                 }
 
             });
-        }
-        else {
-            //count the losses (to draw the stick figure)
-            count++;
-            console.log(`WRONG GUESSES: ${count}`)
-            //TODO DRAW STICK FIGURE
-            if(count == max_guesses) {
-                endGameText.innerHTML = `<h2 class='lost-message'>YOU LOST !!</h2>
-                                        <p>The word was <span>${secretWord}</span></p>`
-                blocker();
             }
-        }
+            else {
+                //lossesCount the losses (to draw the stick figure)
+                lossesCount++;
+                console.log(`WRONG GUESSES: ${lossesCount}`)
+                //TODO DRAW STICK FIGURE
+
+                if(lossesCount == max_guesses) {
+                    endGameText.innerHTML = `<h2 class='lost-message'>YOU LOST !!</h2>
+                                            <p>The word was <span>${secretWord}</span></p>`
+                    disableButtons();
+                }
+            }
+            button.disabled = true;
+        });
+        letterContainer.append(button);
+    }
 
 
-
-
-
-
-
-
-    });
-
-
-
-
-    displayCategories();
 }
 
 
-// // place the correctly guessed letter in the correct position
-// const placeMatchingLetter = (letter, randomWord,underscoreWord) => {
-//     for (let i = 0; i< randomWord.length; i++) {
-//         let currentLetter = randomWord[i]
-//         if (currentLetter === letter) {
-//             underscoreWord
-//[i] = letter;
-//         }
-//     }
-//     return underscoreWord;
-// }
-
-// // check whether the player has won
-// const  hasWon = (sourceWord, newWord) => {
-//     if (sourceWord === newWord.join('')) {
-//         return true;
-//     }
-//     else {
-//         return false;
-//     }
-
-// }
 
 //NEW GAME
-newGameButton.addEventListener("click", initialiser)
-window.onload = initialiser
+newGameButton.addEventListener("click", startGame)
+window.onload = startGame
