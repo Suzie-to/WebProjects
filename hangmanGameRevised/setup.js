7a0beac501f71cb8c294ec1744fe2189df16a809
Suzie-to <yahiya.zyahiya@gmail.com>
2023-07-16 10:31:52 +0100

    added an instructions modal component

--- hangmanGameRevised/setup.js
+++ hangmanGameRevised/setup.js
@@ -10,14 +10,6 @@ const canvas = document.getElementById("canvas");
 const endGameText = document.getElementById("end-game-text")
 
 
-
-let windowRight = document.querySelector('.window-right');
-let windowLeft = document.querySelector('.window-left');
-let roof = document.querySelector('.roof');
-let banner = document.querySelector('h1');
-let doorLeft = document.querySelector('#left-door');
-let doorRight = document.querySelector('#right-door');
-
 const categories = {
     css: ["SELECTOR", "BOX MODEL", "FLEXBOX", "GRID", "RESPONSIVE", 
     "ANIMATION", "TYPOGRAPHY", "GRADIENT", "TRANSITION", "PSEUDO-CLASS"],
@@ -37,7 +29,27 @@ let correctGuesses = 0;
 let wrongGuesses = 0;
 let secretWord = "";
 
+// INSTRUCTIONS
+const slides = Array.from(document.querySelectorAll('.slide'));
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+let closeButton= document.getElementById('close-button');
+const reviewInstructionsButton = document.getElementById('review-instructions-button');
+let manualContainer = document.getElementById('manual-container');
 
+
+let currentSlideIndex = 0;
+
+
+// DRAWING
+let windowRight = document.querySelector('.window-right');
+let windowLeft = document.querySelector('.window-left');
+let roof = document.querySelector('.roof');
+let banner = document.querySelector('h1');
+let doorLeft = document.querySelector('#left-door');
+let doorRight = document.querySelector('#right-door');
+
+/**********************************************************************************/
 const displayCategories = () => {
     categoriesContainer.innerHTML += `<h3>What's your expertise?</h3>`;
     let buttonContainer = document.createElement("div");
@@ -217,6 +229,67 @@ const removeHouseElements = (lossesCount) => {
             break;
     }
 };
+
+
+// INSTRUCTIONS
+function showSlide(index) {
+    // Hide all slides
+    slides.forEach((slide) => {
+      slide.style.display = 'none';
+    });
+  
+    // Show the slide at the specified index
+    slides[index].style.display = 'block';
+  }
+  
+  
+  // Function to go to the previous slide
+  function goToPrevSlide() {
+    currentSlideIndex--;
+    if (currentSlideIndex < 0) {
+      currentSlideIndex = slides.length - 1;
+    }
+    showSlide(currentSlideIndex);
+  }
+  
+  // Function to go to the next slide
+  function goToNextSlide() {
+    currentSlideIndex++;
+    if (currentSlideIndex >= slides.length) {
+      //exit
+      currentSlideIndex = 0;
+    }
+    showSlide(currentSlideIndex);
+  }
+  reviewInstructionsButton.addEventListener('click', function() {
+      manualContainerContainer.style.display = 'block';
+  } );
+  
+  // Function to review the instructions
+  function reviewInstructions() {
+      currentSlideIndex = 0;
+      showSlide(currentSlideIndex);
+    }
+  
+  // Add event listeners to the buttons
+  prevButton.addEventListener('click', goToPrevSlide);
+  nextButton.addEventListener('click', goToNextSlide);
+  reviewInstructionsButton.addEventListener('click', reviewInstructions);
+  
+  // Show the first slide initially
+  showSlide(currentSlideIndex);
+  
+  // close the content when clikcing 'X'
+  closeButton.addEventListener('click', function() {
+      manualContainer.style.display = 'none';
+  });
+  
+  // click anywhere on the window to close the content
+  window.addEventListener('click', function(e) {
+      if (e.target === manualContainer) {
+          manualContainer.style.display = 'none';
+      }
+  });
 
 
 //NEW GAME
--- hangmanGameRevised/testModalOnload/script.js
+++ hangmanGameRevised/testModalOnload/script.js
@@ -1,18 +1,70 @@
 
-let manualContainer = document.getElementById('manual-container');
+const slides = Array.from(document.querySelectorAll('.slide'));
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
 let closeButton= document.getElementById('close-button');
+const reviewInstructionsButton = document.getElementById('review-instructions-button');
+let manualContainer = document.getElementById('manual-container');
 
 
-// EVENT LISTENERS
-// manualContainer.style.visibility = 'visible';
+let currentSlideIndex = 0;
+
+// Function to show the current slide
+function showSlide(index) {
+  // Hide all slides
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+
+  // Show the slide at the specified index
+  slides[index].style.display = 'block';
+}
+
+
+// Function to go to the previous slide
+function goToPrevSlide() {
+  currentSlideIndex--;
+  if (currentSlideIndex < 0) {
+    currentSlideIndex = slides.length - 1;
+  }
+  showSlide(currentSlideIndex);
+}
+
+// Function to go to the next slide
+function goToNextSlide() {
+  currentSlideIndex++;
+  if (currentSlideIndex >= slides.length) {
+    //exit
+    currentSlideIndex = 0;
+  }
+  showSlide(currentSlideIndex);
+}
+reviewInstructionsButton.addEventListener('click', function() {
+    modalContainer.style.display = 'block';
+} );
+
+// Function to review the instructions
+function reviewInstructions() {
+    currentSlideIndex = 0;
+    showSlide(currentSlideIndex);
+  }
+
+// Add event listeners to the buttons
+prevButton.addEventListener('click', goToPrevSlide);
+nextButton.addEventListener('click', goToNextSlide);
+reviewInstructionsButton.addEventListener('click', reviewInstructions);
+
+// Show the first slide initially
+showSlide(currentSlideIndex);
+
 // close the content when clikcing 'X'
 closeButton.addEventListener('click', function() {
-    manualContainer.style.visibility = 'hidden';
+    manualContainer.style.display = 'none';
 });
 
 // click anywhere on the window to close the content
 window.addEventListener('click', function(e) {
     if (e.target === manualContainer) {
-        manualContainer.style.visibility = 'hidden';
+        manualContainer.style.display = 'none';
     }
 });
